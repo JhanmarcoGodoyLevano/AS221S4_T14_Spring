256394251ab865196aeddb23c89b5463fabf1c12
Jhanmarco <jhanmarco.godoy@vallegrande.edu.pe>
2023-09-16 14:49:59 -0500

    proyectoSpring

---
+++ T14_proyectoSpring/src/main/java/com/example/demo/T14ProyectoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T14ProyectoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T14ProyectoSpringApplication.class, args);
+	}
+
+}
